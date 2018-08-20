143b36b5ffe77ebe97e8ca1f186b6f8bc908ae4a
DavidRZuluaga <git@davidrzuluaga.com>
2018-08-20 16:54:09 -0500

    first commit

---
+++ test/models/expense_test.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: expenses
+#
+#  id          :integer          not null, primary key
+#  category_id :integer
+#  date        :date
+#  concept     :string
+#  amount      :decimal(, )
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer
+#
+
+require 'test_helper'
+
+class ExpenseTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
